195cfc70af34977eaec5af98c2bc0caf45c50ad2
Antonio Simón García <antosg@gmail.com>
2019-04-21 23:40:16 +0200

    Voy añadiendo la funcionalidad al alta de grupos

--- src/pages/create-group/create-group.ts
+++ src/pages/create-group/create-group.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { IonicPage, NavController, NavParams, AlertController } from 'ionic-angular';
 
 /**
  * Generated class for the CreateGroupPage page.
@@ -14,11 +14,56 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class CreateGroupPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+ items : Array<{ email: string}>;
+
+  constructor(public navCtrl: NavController,
+    public navParams: NavParams,
+    public alertController: AlertController) {
+
+      this.items = [{"email":"xxxxxxx@gmail.com"},{"email":"yyyyyyy@gmail.com"}];
+
   }
+
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad CreateGroupPage');
   }
+
+  delete(item){
+    console.log("item -> " + item);
+    let pos = this.items.indexOf(item);
+    console.log("pos -> " + pos);
+    this.items.splice(pos, 1);
+  }
+
+  async addInvitation() {
+    let alert = this.alertController.create({
+      title: 'Introduce el mail del usuario al que quieres invitar',
+      inputs: [
+        {
+          name: 'email',
+          placeholder: ''
+        }
+      ],
+      buttons: [
+        {
+          text: 'Cancel',
+          role: 'cancel',
+          handler: data => {
+            console.log('Cancel clicked');
+          }
+        },
+        {
+          text: 'OK',
+          handler: data => {
+            console.log('ok clicked');
+            this.items.push({"email":data.email});
+          }
+        }
+      ]
+    });
+    alert.present();
+  }
+
 
 }
