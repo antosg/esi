793c9f123d6f08c16dd3be7cb09978e699f358d3
Antonio Simón García <antosg@gmail.com>
2019-04-22 13:41:42 +0200

    validaciones al introducir el mail de los invitados
    valido que sea un mail válido en forma y valido que no esté ya en la lista

--- src/pages/create-group/create-group.ts
+++ src/pages/create-group/create-group.ts
@@ -53,7 +53,7 @@ export class CreateGroupPage {
 
   async addInvitation() {
     let alert = this.alertController.create({
-      title: 'Introduce el mail del usuario al que quieres invitar',
+      title: 'Introduce el mail del usuario al que quieres invitar a participar en tu grupo',
       inputs: [
         {
           name: 'email',
@@ -72,12 +72,52 @@ export class CreateGroupPage {
           text: 'OK',
           handler: data => {
             console.log('ok clicked');
-            this.items.push({"email":data.email});
+            if (this.checkEmail(data.email)){
+              if (this.items.indexOf(data) == 0){
+                this.items.push({"email":data.email});
+              }else{
+                let alert = this.alertCtrl.create({
+                  title:'^Invitation Error',
+                  subTitle:"The email already exists!",
+                  buttons:['OK']
+                });
+                alert.present();
+                return;
+              }
+            }else{
+              let alert = this.alertCtrl.create({
+                title:'^Invitation Error',
+                subTitle:"It's not a valid email",
+                buttons:['OK']
+              });
+              alert.present();
+              return;
+            }
           }
         }
       ]
     });
     alert.present();
+  }
+
+  checkEmail(emailAddress) {
+    var sQtext = '[^\\x0d\\x22\\x5c\\x80-\\xff]';
+    var sDtext = '[^\\x0d\\x5b-\\x5d\\x80-\\xff]';
+    var sAtom = '[^\\x00-\\x20\\x22\\x28\\x29\\x2c\\x2e\\x3a-\\x3c\\x3e\\x40\\x5b-\\x5d\\x7f-\\xff]+';
+    var sQuotedPair = '\\x5c[\\x00-\\x7f]';
+    var sDomainLiteral = '\\x5b(' + sDtext + '|' + sQuotedPair + ')*\\x5d';
+    var sQuotedString = '\\x22(' + sQtext + '|' + sQuotedPair + ')*\\x22';
+    var sDomain_ref = sAtom;
+    var sSubDomain = '(' + sDomain_ref + '|' + sDomainLiteral + ')';
+    var sWord = '(' + sAtom + '|' + sQuotedString + ')';
+    var sDomain = sSubDomain + '(\\x2e' + sSubDomain + ')*';
+    var sLocalPart = sWord + '(\\x2e' + sWord + ')*';
+    var sAddrSpec = sLocalPart + '\\x40' + sDomain; // complete RFC822 email address spec
+    var sValidEmail = '^' + sAddrSpec + '$'; // as whole string
+
+    var reValidEmail = new RegExp(sValidEmail);
+
+    return reValidEmail.test(emailAddress);
   }
 
   doRegister() {
