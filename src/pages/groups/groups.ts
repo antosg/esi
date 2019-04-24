be5b9f51118c68a51b80f1af5317e5d0c8e40e36
Antonio Simón García <antosg@gmail.com>
2019-04-24 17:01:11 +0200

    Finalizar grupo. hago las últimas modificaciones

--- src/pages/groups/groups.ts
+++ src/pages/groups/groups.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams, AlertController, LoadingController} from 'ionic-angular';
 import { Http } from '@angular/http';
-import { TransactionService } from '../../app/_services/index';
+import { TransactionService, AuthService } from '../../app/_services/index';
 import { IDetailedError } from '@ionic/cloud-angular';
 
 import { StatisticsPage } from '../../pages/statistics/statistics';
@@ -32,7 +32,8 @@ export class GroupsPage {
     public http: Http,
     public alertCtrl: AlertController,
     public loadingCtrl:LoadingController,
-    public TransactionService: TransactionService) {
+    public TransactionService: TransactionService,
+    public AuthService : AuthService) {
 
       this.ionViewDidLoad();
 
@@ -103,7 +104,33 @@ export class GroupsPage {
         }, {
           text: '^Confirmar',
           handler: () => {
-            console.log('Confirm Okay');
+            console.log('Confirmar que quieres cerrar el grupo -> ' + grupo._id);
+            var j = new Date();
+            grupo.fecfin = j;
+            this.AuthService.updateGroup(grupo).then((registerGroupRs) => {
+              console.log('ok acceptOrNotInvitation');
+              //let registerData: any = registerResult;
+              //this.RegisterInvitationsRs = registerData;
+              //console.log("Datos -> " + JSON.stringify(this.RegisterInvitationsRs));
+              //this.invitations = this.RegisterInvitationsRs;
+              //this.delete(grupo);
+            }, (err:IDetailedError<string[]>) => {
+              let errors = '';
+              for(let e of err.details) {
+                console.log(e);
+                if(e === 'required_email') errors += '^Email is required.<br/>';
+                if(e === 'required_password') errors += '^Password is required.<br/>';
+                if(e === 'conflict_email') errors += '^A user with this email already exists.<br/>';
+                //don't need to worry about conflict_username
+                if(e === 'invalid_email') errors += 'Your email address isn\'t valid.';
+              }
+              let alert = this.alertCtrl.create({
+                title:'^Finalice group error',
+                subTitle:errors,
+                buttons:['OK']
+              });
+              alert.present();
+            });
           }
         }
       ]
