385709827cbdec745f93115b2e3164335bcda4d8
Antonio Simón García <antosg@gmail.com>
2019-04-27 19:45:13 +0200

    cambio las rutas para generar la .apk

--- publishApp.bat
+++ publishApp.bat
@@ -1,14 +1,14 @@
 echo off
 	echo Comienza la creacion de la app
 		echo borrando las ultimas versines que se crearon
-			del C:\Users\antos\Antonio\reponline\esi\platforms\android\app\build\outputs\apk\release\app-release-unsigned.apk
-			del C:\Users\antos\Antonio\reponline\esi\platforms\android\app\build\outputs\apk\release\esi.apk
+			del C:\Users\antos\Antonio\reponline\esiApp\esi\platforms\android\app\build\outputs\apk\release\app-release-unsigned.apk
+			del C:\Users\antos\Antonio\reponline\esiApp\esi\platforms\android\app\build\outputs\apk\release\esi.apk
 		echo borradas la ultimas versiones que se crearon
 		echo creando la release
 			ionic cordova build --release android
 		echo fin de la creacion de la release
 		echo firmando la release
-			cd C:\Users\antos\Antonio\reponline\esi\platforms\android\app\build\outputs\apk\release
+			cd C:\Users\antos\Antonio\reponline\esiApp\esi\platforms\android\app\build\outputs\apk\release
 			jarsigner -verbose -sigalg SHA1withRSA -digestalg SHA1 -keystore my-release-key.keystore app-release-unsigned.apk alias_name
 		echo fin firma release
 		echo creando la app
